8db06d896dc1136b3eb1d831955aa372f96b62d3
ichatss <il.st13@yandex.ru>
2020-03-14 23:34:48 +0300

    Доработана модель Артура

--- src/main/java/com/tinychiefdelights/Artur.java
+++ src/main/java/com/tinychiefdelights/Artur.java
@@ -3,4 +3,9 @@ package com.tinychiefdelights;
 public class Artur {
     private String name = "Arthur";
     private String lastName = "Vartanyan";
+
+    public void sdelatZamechanie()
+    {
+
+    }
 }
